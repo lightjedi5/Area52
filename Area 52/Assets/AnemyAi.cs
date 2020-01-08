a187aa4500a8b5e0bd76b22d44eae23596448c69
jerry platero <jerryplatero@hotmail.com>
2020-01-08 07:13:56 -0700

    uploaded Area 52

---
+++ Area 52/Assets/AnemyAi.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnemyAi : MonoBehaviour
+{
+    public float speed;
+
+    private Transform target;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed + Time.deltaTime);
+    }
+}
